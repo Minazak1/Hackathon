2e6e6edd8757f714c293d9510f635ab2884cb14f
Nikhil Patel <patel.nikhil1921@gmail.com>
2020-11-20 21:25:10 -0600

    1st test

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function smipleMathProblemGenerator() {
     var a = Math.floor(Math.random() * 10) + 1;
     var b = Math.floor(Math.random() * 10) + 1;
     console.log(a + randomElement + b);
-    document.getElementById("equation").innerHTML = a + randomElement + b
     }
     smipleMathProblemGenerator();
+
+function test(p1)
+{
+    return p1;
+}
