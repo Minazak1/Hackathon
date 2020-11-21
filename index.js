3dcb9e83fffe7047bce056d00c20623987e31e36
AnisJah <65712890+AnisJah@users.noreply.github.com>
2020-11-21 06:43:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,37 +11,24 @@ function simpleMathProblemGenerator() {
         answer = (a-b);
     console.log(answer);
     }
-    function simpleMathProblemGenerator() {
-        var answer;
-        arr = ["+","-"];
-        randomElement = arr[Math.floor(Math.random() * arr.length)];
-        var a = Math.floor(Math.random() * 10) + 1;
-        var b = Math.floor(Math.random() * 10) + 1;
-        console.log(a + randomElement + b);
-        if(randomElement == "+")
-            answer = (a+b);
-        if(randomElement == "-")
-            answer = (a-b);
-        console.log(answer);
-        }
-        function normalMathProblemGenerator() {
-            answer = 0;
-            arr = ["+","-","*","/"];
-            randomElement = arr[Math.floor(Math.random() * arr.length)];
-            var a = Math.floor(Math.random() * 100) + 1;
-            var b = Math.floor(Math.random() * 10) + 1;
-            console.log(a + randomElement + b);
-            if(randomElement == "+")
-            answer = (a+b);
-            if(randomElement == "-")
-            answer = (a-b);
-            if(randomElement == "*")
-            answer = (a*b);
-            if(randomElement == "/")
-            {
-            answer = (a/b);
-            if (answer%2 !=0)
-               normalMathProblemGenerator();
-            }
-            console.log(answer);
-            }
+function normalMathProblemGenerator() {
+    answer = 0;
+    arr = ["+","-","*","/"];
+    randomElement = arr[Math.floor(Math.random() * arr.length)];
+    var a = Math.floor(Math.random() * 100) + 1;
+    var b = Math.floor(Math.random() * 10) + 1;
+    console.log(a + randomElement + b);
+    if(randomElement == "+")
+    answer = (a+b);
+    if(randomElement == "-")
+    answer = (a-b);
+    if(randomElement == "*")
+    answer = (a*b);
+    if(randomElement == "/")
+    {
+    answer = (a/b);
+    if (answer%2 !=0)
+    normalMathProblemGenerator();
+    }
+    console.log(answer);
+    }
