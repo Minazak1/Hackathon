b8cc91899aa2c29d6e0396599c34574af6d79416
Mina Zaky <Minazak1@users.noreply.github.com>
2020-11-20 22:22:03 -0500

    Math
    Added a random math display for random equations

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function smipleMathProblemGenerator() {
     var a = Math.floor(Math.random() * 10) + 1;
     var b = Math.floor(Math.random() * 10) + 1;
     console.log(a + randomElement + b);
+    document.getElementById("equation").innerHTML = a + randomElement + b
     }
     smipleMathProblemGenerator();
