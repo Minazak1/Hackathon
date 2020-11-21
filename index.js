11296a69c85294860bd604d45f7682269ec77ed4
AnisJah <65712890+AnisJah@users.noreply.github.com>
2020-11-21 05:15:33 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function smipleMathProblemGenerator() {
+    arr = ["+","-"];
+    randomElement = arr[Math.floor(Math.random() * arr.length)];
+    var a = Math.floor(Math.random() * 10) + 1;
+    var b = Math.floor(Math.random() * 10) + 1;
+    console.log(a + randomElement + b);
+    }
+    smipleMathProblemGenerator();
