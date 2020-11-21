1c2d9789c618d6d770b5a1150be2e76ebadec6a9
AnisJah <65712890+AnisJah@users.noreply.github.com>
2020-11-21 05:59:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function simpleMathProblemGenerator() {
+    var answer;
     arr = ["+","-"];
     randomElement = arr[Math.floor(Math.random() * arr.length)];
     var a = Math.floor(Math.random() * 10) + 1;
     var b = Math.floor(Math.random() * 10) + 1;
     console.log(a + randomElement + b);
+    if(randomElement == "+")
+        answer = (a+b);
+    if(randomElement == "-")
+        answer = (a-b);
+    console.log(answer);
     }
 function normalMathProblemGenerator() {
     arr = ["+","-","*","/"];
     randomElement = arr[Math.floor(Math.random() * arr.length)];
     var a = Math.floor(Math.random() * 100) + 1;
-    var b = Math.floor(Math.random() * 100) + 1;
+    var b = Math.floor(Math.random() * 10) + 1;
     console.log(a + randomElement + b);
     }
+    simpleMathProblemGenerator();
